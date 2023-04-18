ba8eab8acf3147181594bd08d7ea7fea809ff984
Eugene Birondo <jbirondo@gmail.com>
2023-04-18 12:29:02 -0700

    deleting comments

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
   <div>
   <head>
-		<title>Eugene Birondo</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
